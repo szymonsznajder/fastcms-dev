87e6114a28f544eb6b422193f35ca6d8b05a33f8
ComwrapUkReply <s.sznajder@reply.com>
2024-06-15 21:03:22 +0100

    remove plus plus

--- plusplus/src/samples/blocks/text/text.js
+++
@@ -1,5 +0,0 @@
-import { renderExpressions } from "/plusplus/plugins/expressions/src/expressions.js";
-
-export default function decorate(block) {
-  renderExpressions(document.querySelector('.text-wrapper'));
-}
