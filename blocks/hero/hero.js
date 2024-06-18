c8b88df41d630006b5dd55938512ee0a44ca9e3e
ComwrapUkReply <s.sznajder@reply.com>
2024-06-18 11:56:10 +0100

    hero parallax try

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -22,5 +22,16 @@ export default function decorate(block) {
       imageParent.replaceWith(newDiv);
     }
 
+    window.addEventListener("scroll", function() {
+      const distance = window.scrollY;
+      document.querySelector(".hero").style.transform = `translateY(${distance *
+        1}px)`;
+      document.querySelector(
+        ".hero-item"
+      ).style.transform = `translateY(${distance * 0.3}px)`;
+      setTimeout(() => {
+        document.querySelector("section h3").classList.add("animate-me");
+      }, 400);
+    });
   }
 )};
